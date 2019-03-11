44df1c55f5ac645d859681711ccc3d7dc82f14b1
ruizhang <ruizhang1995@tamu.edu>
2019-03-11 16:42:18 -0500

    Finished First part

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The code provided will print ‘Hello World’ to the console.
+            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
+            int number = 1;
+            int gou = 2;
+            Console.WriteLine(number+gou);
+            //Console.ReadKey();
+
+            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+        }
+    }
+}
